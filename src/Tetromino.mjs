aa8b9a2cb3802d977f9fed1cb34bc00535a7206c
BorisBanchev <boris.banchev03@gmail.com>
2025-12-29 19:37:58 +0200

    Refactor Tetromino class to not call on each shape fromString function

--- src/Tetromino.mjs
+++ src/Tetromino.mjs
@@ -30,40 +30,41 @@ export class Tetromino {
     return new Tetromino(prev, this.orientations);
   }
 }
-Tetromino.T_SHAPE = Tetromino.fromString(0, 4, `
+const def = (current, count, s) => Tetromino.fromString(current, count, s);
+Tetromino.T_SHAPE = def(0, 4, `
     .T.
     TTT
     ...
     `)
 
-Tetromino.I_SHAPE = Tetromino.fromString(0, 2, `
+Tetromino.I_SHAPE = def(0, 2, `
   .....
   .....
   IIII.
   .....
   .....
   `)
-Tetromino.O_SHAPE = Tetromino.fromString(0, 1, `
+Tetromino.O_SHAPE = def(0, 1, `
   .OO
   .OO
   ...
   `)
-Tetromino.L_SHAPE = Tetromino.fromString(0, 4, `
+Tetromino.L_SHAPE = def(0, 4, `
   ..L
   LLL
   ...
   `)
-Tetromino.J_SHAPE = Tetromino.fromString(0, 4, `
+Tetromino.J_SHAPE = def(0, 4, `
   J..
   JJJ
   ...
   `)
-Tetromino.S_SHAPE = Tetromino.fromString(0,2, `
+Tetromino.S_SHAPE = def(0,2, `
   .SS
   SS.
   ...
   `)
-Tetromino.Z_SHAPE = Tetromino.fromString(0,2, `
+Tetromino.Z_SHAPE = def(0,2, `
   ZZ.
   .ZZ
   ...

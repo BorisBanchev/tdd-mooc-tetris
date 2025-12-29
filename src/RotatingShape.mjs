1400ed13ed46d8d1005bdceed23f2789315ca49b
BorisBanchev <boris.banchev03@gmail.com>
2025-12-29 14:15:34 +0200

    Make initial orientation test pass

--- src/RotatingShape.mjs
+++ src/RotatingShape.mjs
@@ -2,4 +2,12 @@ export class RotatingShape {
   constructor(grid) {
     this.grid = grid
   }
+  static fromString(s) {
+    const lines = s.split('\n').map(l => l.trim()).filter(l => l.length)
+    const grid = lines.map(line => line.split(''))
+    return new RotatingShape(grid)
+  }
+  toString() {
+    return this.grid.map(row => row.join('')).join('\n') + '\n'
+  }
 }

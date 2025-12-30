3bffbbbc0baafdfb76779ca8ea39e30b6ea017ea
BorisBanchev <boris.banchev03@gmail.com>
2025-12-30 15:19:53 +0200

    Implement a failing test for moving falling tetromino left

---
+++ test/MovingTetrominoes.test.mjs
@@ -0,0 +1,35 @@
+import { beforeEach, describe, test } from "vitest";
+import { expect } from "chai";
+import { Board } from "../src/Board.mjs";
+import { Tetromino } from "../src/Tetromino.mjs";
+
+describe("Moving falling tetrominoes", () => {
+  let board
+  beforeEach(() => {
+    board = new Board(10, 6)
+  })
+  test("start from the top middle", () => {
+    board.drop(Tetromino.T_SHAPE);
+
+    expect(board.toString()).to.equalShape(
+      `....T.....
+       ...TTT....
+       ..........
+       ..........
+       ..........
+       ..........`
+    );
+  });
+  test("a falling tetromino can be moved left", () => {
+    board.drop(Tetromino.T_SHAPE)
+    board.moveLeft()
+    expect(board.toString()).to.equalShape(
+      `...T......
+       ..TTT.....
+       ..........
+       ..........
+       ..........
+       ..........`
+    )
+  })
+})

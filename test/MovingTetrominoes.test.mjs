bb1ea2e0f5838b9c664b216d7103871cd81a64f2
BorisBanchev <boris.banchev03@gmail.com>
2025-12-30 16:15:31 +0200

    Add a test if a falling block can be moved right

--- test/MovingTetrominoes.test.mjs
+++ test/MovingTetrominoes.test.mjs
@@ -32,4 +32,17 @@ describe("Moving falling tetrominoes", () => {
        ..........`
     )
   })
+  test("a falling tetromino can be moved right", () => {
+    board.drop(Tetromino.T_SHAPE);
+    board.moveRight();
+
+    expect(board.toString()).to.equalShape(
+      `.....T....
+       ....TTT...
+       ..........
+       ..........
+       ..........
+       ..........`
+    );
+  });
 })
